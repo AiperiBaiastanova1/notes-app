330ab0e1de4ab275a75611f8ddd03cc03f6e7456
Aiperi <aiperi@mail.ru>
2023-05-25 15:57:32 +0200

    Init

---
+++ src/components/HtmlForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form action="#">
+    <div>
+      <p>Name</p>
+      <input tabindex="1" placeholder="Name" type="text">
+    </div>
+    <div>
+      <p>Password</p>
+      <input tabindex="2" type="password">
+    </div>
+    <div>
+      <p>Phone</p>
+      <input tabindex="3" type="tel">
+    </div>
+    <div>
+      <p>Gender</p>
+      <div>
+        <input tabindex="4" type="radio"> Man
+      </div>
+      <div>
+        <input tabindex="4" type="radio"> Woman
+      </div>
+    </div>
+  </form>
+</template>
